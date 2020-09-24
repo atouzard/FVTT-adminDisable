61f5701f9c017e34a31b1acb900b6e71c8b3e51d
Arthur TOUZARD <atouzardpro@gmail.com>
2020-09-24 17:29:21 +0200

    Update disable-admin.js

--- disable-admin.js
+++ disable-admin.js
@@ -3,7 +3,6 @@ let userIdsToShowItems = [ "5d24f4ba6154fxGM", "5d44a07c9fc5exGM" ];
 // Remove settings to configure modules or exit to the setup screen.
 Hooks.on("renderSettings", (app, html) => {
   if (!userIdsToShowItems .includes(game.userId)) {
-    html.find('button[data-action="configure"]').remove();
     html.find('button[data-action="setup"]').remove();
     html.find('button[data-action="modules"]').remove();
     html.find('button[data-action="players"]').remove();
